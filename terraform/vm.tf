02b4670e558a15dd85affac9426fbcddb95921b4
Ermac <ermac@inbox.ru>
2024-05-12 18:14:24 +0000

    test md

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -34,6 +34,40 @@ resource "yandex_compute_instance" "vm-web-a" {
   }
 }
 
+#бкап машины web-a
+resource "yandex_compute_snapshot" "vm-web-a-snapshot" {
+  name        = "vm-web-a-snapshot-${formatdate("YYYYMMDD", timestamp())}"
+  description = "Daily snapshot of vm-web-a"
+  source_disk_id= "${yandex_compute_instance.vm-web-a.boot_disk.0.disk_id}"
+
+  labels = {
+    "auto-delete" = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "yandex_compute_snapshot_schedule" "vm-web-a-snapshot-schedule" {
+  name           = "vm-web-a-snapshot-schedule"
+  description    = "Schedule for daily snapshots of vm-web-a with one-week retention"
+
+  schedule_policy {
+    expression = "0 0 * * *"
+  }
+
+  retention_period = "168h" // 7 days in hours
+
+  snapshot_spec {
+    description = "Snapshot created by schedule"
+    labels = {
+      snapshot-label = "vm-web-a"
+    }
+  }
+
+  disk_ids = [yandex_compute_instance.vm-web-a.boot_disk.0.disk_id]
+}
+
 #веб 2
 resource "yandex_compute_instance" "vm-web-b" {
   name        = "vm-web-b"
@@ -66,6 +100,41 @@ resource "yandex_compute_instance" "vm-web-b" {
     preemptible = true
   }
 }
+
+#бкап машины web-b
+resource "yandex_compute_snapshot" "vm-web-b-snapshot" {
+  name        = "vm-web-b-snapshot-${formatdate("YYYYMMDD", timestamp())}"
+  description = "Daily snapshot of vm-web-b"
+  source_disk_id= "${yandex_compute_instance.vm-web-b.boot_disk.0.disk_id}"
+
+  labels = {
+    "auto-delete" = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "yandex_compute_snapshot_schedule" "vm-web-b-snapshot-schedule" {
+  name           = "vm-web-b-snapshot-schedule"
+  description    = "Schedule for daily snapshots of vm-web-b with one-week retention"
+
+  schedule_policy {
+    expression = "0 0 * * *"
+  }
+
+  retention_period = "168h" // 7 days in hours
+
+  snapshot_spec {
+    description = "Snapshot created by schedule"
+    labels = {
+      snapshot-label = "vm-web-b"
+    }
+  }
+
+  disk_ids = [yandex_compute_instance.vm-web-b.boot_disk.0.disk_id]
+}
+
 #Elastics
 resource "yandex_compute_instance" "vm-elastics" {
   name        = "vm-elastics"
@@ -97,6 +166,40 @@ resource "yandex_compute_instance" "vm-elastics" {
   scheduling_policy {
     preemptible = true
   }
+}
+
+#бкап машины elastics
+resource "yandex_compute_snapshot" "vm-elastics-snapshot" {
+  name        = "vm-elastics-snapshot-${formatdate("YYYYMMDD", timestamp())}"
+  description = "Daily snapshot of vm-elastics"
+  source_disk_id= "${yandex_compute_instance.vm-elastics.boot_disk.0.disk_id}"
+
+  labels = {
+    "auto-delete" = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "yandex_compute_snapshot_schedule" "vm-elastics-snapshot-schedule" {
+  name           = "vm-elastics-snapshot-schedule"
+  description    = "Schedule for daily snapshots of vm-elastics with one-week retention"
+
+  schedule_policy {
+    expression = "0 0 * * *"
+  }
+
+  retention_period = "168h" // 7 days in hours
+
+  snapshot_spec {
+    description = "Snapshot created by schedule"
+    labels = {
+      snapshot-label = "vm-elastics-daily"
+    }
+  }
+
+  disk_ids = [yandex_compute_instance.vm-elastics.boot_disk.0.disk_id]
 }
 
 #Zabbix
@@ -132,6 +235,39 @@ resource "yandex_compute_instance" "vm-zabbix" {
   }
 }
 
+#бкап машины zabbix
+resource "yandex_compute_snapshot" "vm-zabbix-snapshot" {
+  name        = "vm-zabbix-snapshot-${formatdate("YYYYMMDD", timestamp())}"
+  description = "Daily snapshot of vm-elastics"
+  source_disk_id= "${yandex_compute_instance.vm-zabbix.boot_disk.0.disk_id}"
+
+  labels = {
+    "auto-delete" = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "yandex_compute_snapshot_schedule" "vm-zabbix-snapshot-schedule" {
+  name           = "vm-zabbix-snapshot-schedule"
+  description    = "Schedule for daily snapshots of vm-zabbix with one-week retention"
+
+  schedule_policy {
+    expression = "0 0 * * *"
+  }
+
+  retention_period = "168h" // 7 days in hours
+
+  snapshot_spec {
+    description = "Snapshot created by schedule"
+    labels = {
+      snapshot-label = "vm-zabbix-daily"
+    }
+  }
+
+  disk_ids = [yandex_compute_instance.vm-zabbix.boot_disk.0.disk_id]
+}
 
 #Kibana
 resource "yandex_compute_instance" "vm-kibana" {
@@ -167,7 +303,39 @@ resource "yandex_compute_instance" "vm-kibana" {
   }
 }
 
+#бкап машины kibana
+resource "yandex_compute_snapshot" "vm-kibana-snapshot" {
+  name        = "vm-kibana-snapshot-${formatdate("YYYYMMDD", timestamp())}"
+  description = "Daily snapshot of vm-elastics"
+  source_disk_id= "${yandex_compute_instance.vm-kibana.boot_disk.0.disk_id}"
 
+  labels = {
+    "auto-delete" = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "yandex_compute_snapshot_schedule" "vm-kibana-snapshot-schedule" {
+  name           = "vm-kibana-snapshot-schedule"
+  description    = "Schedule for daily snapshots of vm-kibana with one-week retention"
+
+  schedule_policy {
+    expression = "0 0 * * *"
+  }
+
+  retention_period = "168h" // 7 days in hours
+
+  snapshot_spec {
+    description = "Snapshot created by schedule"
+    labels = {
+      snapshot-label = "vm-kibana-daily"
+    }
+  }
+
+  disk_ids = [yandex_compute_instance.vm-kibana.boot_disk.0.disk_id]
+}
 
 #Бастион
 
@@ -205,4 +373,38 @@ resource "yandex_compute_instance" "bastion-host" {
   scheduling_policy {
     preemptible = true
   }
+}
+
+#бкап машины bastion-host
+resource "yandex_compute_snapshot" "bastion-host-snapshot" {
+  name        = "bastion-host-snapshot-${formatdate("YYYYMMDD", timestamp())}"
+  description = "Daily snapshot of vm-bastion-host"
+  source_disk_id= "${yandex_compute_instance.bastion-host.boot_disk.0.disk_id}"
+
+  labels = {
+    "auto-delete" = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "yandex_compute_snapshot_schedule" "vm-bastion-host-snapshot-schedule" {
+  name           = "bastion-host-snapshot-schedule"
+  description    = "Schedule for daily snapshots of bastion-host with one-week retention"
+
+  schedule_policy {
+    expression = "0 0 * * *"
+  }
+
+  retention_period = "168h" // 7 days in hours
+
+  snapshot_spec {
+    description = "Snapshot created by schedule"
+    labels = {
+      snapshot-label = "vm-bastion-host-daily"
+    }
+  }
+
+  disk_ids = [yandex_compute_instance.bastion-host.boot_disk.0.disk_id]
 }
